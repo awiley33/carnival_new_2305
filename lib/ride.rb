c5bd3fb8fae9de8c20f47039152025a83560b784
Anna Wiley <awiley33@gmail.com>
2023-06-13 11:16:59 -0500

    feat: Add board rider method, define an empty hash in rider log

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,13 +2,20 @@ class Ride
   attr_reader :name,
               :min_height,
               :admission_fee,
-              :excitement
+              :excitement,
+              :rider_log,
+              :total_revenue
 
   def initialize(info)
     @name = info[:name]
     @min_height = info[:min_height]
     @admission_fee = info[:admission_fee]
     @excitement = info[:excitement]
+    @rider_log = Hash.new(0)
+    @total_revenue = 0
+  end
 
+  def board_rider(rider)
+    @rider_log[rider] += 1
   end
 end
