1bdc10c83bfdcff026305f2f6470191bf12d675f
Anna Wiley <awiley33@gmail.com>
2023-06-13 10:23:27 -0500

    test: Comment out not yet used lib files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ require 'date'
 require 'pry'
 require 'RSpec'
 require './lib/visitor'
-require './lib/ride'
-require './lib/carnival'
+# require './lib/ride'
+# require './lib/carnival'
 
 RSpec.configure do |config|
   config.formatter = :documentation
