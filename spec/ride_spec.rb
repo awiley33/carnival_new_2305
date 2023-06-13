53201169ed4b53b393597f4068b4e828feb1cc43
Anna Wiley <awiley33@gmail.com>
2023-06-13 11:16:23 -0500

    test: Add test for board rider and rider log

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -14,5 +14,22 @@ describe Ride do
     expect(@ride1.min_height).to eq(24)
     expect(@ride1.admission_fee).to eq(1)
     expect(@ride1.excitement).to eq(:gentle)
+    expect(@ride1.total_revenue).to eq(0)
+  end
+
+  it "has a rider log that tracks who has ridden the ride and how many times" do
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+
+    expected = {
+      @visitor1 => 2,
+      @visitor2 => 1
+    }
+
+    expect(@ride1.rider_log).to eq(expected)
   end
 end
