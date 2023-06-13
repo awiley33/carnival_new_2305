c4826b7818f9dad95c401ec2252552bcd7cdddab
Anna Wiley <awiley33@gmail.com>
2023-06-13 12:38:05 -0500

    test: Add test for carnival#most_profitable_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -42,10 +42,30 @@ describe Carnival do
     @ride1.board_rider(@visitor2)
     @ride1.board_rider(@visitor1)
     @ride1.board_rider(@visitor1)
-    @ride2.board_rider(@visitor3)
-    @ride2.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3)
 
     expect(@carnival1.most_popular_ride).to eq(@ride1)
+  end
+
+  it "can tell us its most profitable ride" do
+    @carnival1.add_ride(@ride1)
+    @carnival1.add_ride(@ride2)
+    @carnival1.add_ride(@ride3)
+
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor3.add_preference(:thrilling)
+
+    @ride1.board_rider(@visitor1) 
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor1) # $4
+    @ride3.board_rider(@visitor3)
+    @ride3.board_rider(@visitor3) # $4
+    @ride2.board_rider(@visitor2) # $5
+    
+    expect(@carnival1.most_profitable_ride).to eq(@ride2)
   end
 
 end
