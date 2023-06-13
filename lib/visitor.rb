22f9e3aac6e7896ec044499c2e4712a22a8dec53
Anna Wiley <awiley33@gmail.com>
2023-06-13 10:30:09 -0500

    feat: Add prefences to instance vara and attr reader, define add preference method

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,14 +1,20 @@
 class Visitor
   attr_reader :name,
-              :height
+              :height,
+              :preferences
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
     @spending_money = spending_money
+    @preferences = []
   end
 
   def spending_money
     (@spending_money.delete("$")).to_i
+  end
+
+  def add_preference(pref)
+    @preferences.push(pref)
   end
 end
