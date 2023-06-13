62be88b12a02a8df7600b5106360d7a43166d132
Anna Wiley <awiley33@gmail.com>
2023-06-13 12:33:06 -0500

    test: Add test for carnival#most_popular_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -29,4 +29,23 @@ describe Carnival do
     expect(@carnival1.rides).to eq([@ride1, @ride2, @ride3])
   end
 
+  it "can tell us its most popular ride" do
+    @carnival1.add_ride(@ride1)
+    @carnival1.add_ride(@ride2)
+    @carnival1.add_ride(@ride3)
+
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor3.add_preference(:thrilling)
+
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor2)
+    @ride1.board_rider(@visitor1)
+    @ride1.board_rider(@visitor1)
+    @ride2.board_rider(@visitor3)
+    @ride2.board_rider(@visitor3)
+
+    expect(@carnival1.most_popular_ride).to eq(@ride1)
+  end
+
 end
