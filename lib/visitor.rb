7ff0fc775eefb329fb17f51023fd6b7f1532bfce
Anna Wiley <awiley33@gmail.com>
2023-06-13 10:24:32 -0500

    feat: Initialize Visitor class with attr reader and spending money var conversion

---
+++ lib/visitor.rb
@@ -0,0 +1,14 @@
+class Visitor
+  attr_reader :name,
+              :height
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money
+  end
+
+  def spending_money
+    (@spending_money.delete("$")).to_i
+  end
+end
