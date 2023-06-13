31bba5bb891b0ae30162f16cdaf21a4e37594582
Anna Wiley <awiley33@gmail.com>
2023-06-13 10:39:15 -0500

    test: Add test for Visitor tall enough method

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -4,6 +4,8 @@ require "spec_helper"
 describe Visitor do
   before(:each) do
     @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
 
 
@@ -21,5 +23,12 @@ describe Visitor do
     @visitor1.add_preference(:thrilling)
     
     expect(@visitor1.preferences).to eq([:gentle, :thrilling])
+  end
+
+  it "can check if they are tall enough for rides based on a given height threshold" do
+    expect(@visitor1.tall_enough?(54)).to be true
+    expect(@visitor2.tall_enough?(54)).to be false
+    expect(@visitor3.tall_enough?(54)).to be true
+    expect(@visitor1.tall_enough?(64)).to be false
   end
 end
