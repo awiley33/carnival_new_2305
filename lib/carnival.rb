e0dc548900d7ee60e75f8bb8ef37624edb9c2c51
Anna Wiley <awiley33@gmail.com>
2023-06-13 12:38:25 -0500

    feat: Add method carnival#most_profitable_ride

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -14,4 +14,8 @@ class Carnival
   def most_popular_ride
     @rides.max_by { |ride| ride.ride_count}
   end
+
+  def most_profitable_ride
+    @rides.max_by { |ride| ride.total_revenue}
+  end
 end
