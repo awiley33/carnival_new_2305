3483e0d84a1f3459281cc0b2981a760da3dd3e1f
Anna Wiley <awiley33@gmail.com>
2023-06-13 12:19:20 -0500

    test: Add test for adding rides

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -19,4 +19,14 @@ describe Carnival do
     expect(@carnival1.rides).to eq([])
   end
 
+  it "starts with an empty rides array and can add rides" do
+    expect(@carnival1.rides).to eq([])
+
+    @carnival1.add_ride(@ride1)
+    @carnival1.add_ride(@ride2)
+    @carnival1.add_ride(@ride3)
+
+    expect(@carnival1.rides).to eq([@ride1, @ride2, @ride3])
+  end
+
 end
